c5f72cc776a335b158bfbbede8679b856615d92a
SerAta26 <atamassergey26@gmail.com>
2025-10-28 20:07:56 +0300

    Lab_1_4

--- project/settings.py
+++ project/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
     'apps.db_train_alternative',
     'debug_toolbar',
-    'rest_framework'
+    'rest_framework',
+    'django_filters'
 ]
 
 
@@ -71,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -155,11 +156,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-import mimetypes
-
-mimetypes.add_type("application/javascript", ".js", True)
-
-DEBUG_TOOLBAR_CONFIG = {
-    "INTERCEPT_REDIRECTS": False,
-}
